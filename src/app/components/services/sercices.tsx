4255f0877d036a874c022fdf110b8e60a78fcfc6
yitethio <yitbaandu703@gmail.com>
2025-02-24 18:53:36 +0300

    worked on the cards expanding and colaps

--- src/app/components/services/sercices.tsx
+++ src/app/components/services/sercices.tsx
@@ -16,7 +16,7 @@ const Services = () => {
         {data.services.map((service) => (
           <div
             key={service.title}
-            className="relative w-full sm:w-1/2 lg:w-1/4 h-[300px] lg:h-[400px] bg-cover bg-center group"
+            className="relative w-full sm:w-1/2 lg:w-1/4 h-[350px] lg:h-[450px] bg-cover bg-center group"
             style={{ backgroundImage: `url(${service.image})` }}
           >
             <div className="absolute inset-0 bg-[#500114]/70 flex flex-col p-5 text-white transition-all duration-300 group-hover:h-auto">
@@ -28,9 +28,6 @@ const Services = () => {
                 <p className="text-xs lg:text-sm opacity-80 group-hover:opacity-100 group-hover:line-clamp-none line-clamp-3">
                   {service.description}
                 </p>
-
-                {/* Gradient overlay when not hovered */}
-                <div className="absolute bottom-0 left-0 w-full h-10 bg-gradient-to-t from-[#500114] to-transparent opacity-100 group-hover:opacity-0 transition-opacity duration-300"></div>
               </div>
             </div>
           </div>
